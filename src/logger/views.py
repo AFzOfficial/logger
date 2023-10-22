26b4529f20af48dbf90309c488b358df32920a5f
AFzOfficial <abol.mortal50@gmail.com>
2023-10-22 18:35:34 +0330

    refactor: changing the imports order

--- src/logger/views.py
+++ src/logger/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .decorators import guest_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.core.paginator import Paginator
 from django.contrib import messages
 
 from .models import Log
+from .decorators import guest_required
 from .forms import LogForm, SignUpForm, UpdateUserForm, ProfileForm
 
 
