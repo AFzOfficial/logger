8c6d96d12eb1f402def6f916ffa4a96cc791f233
AFzOfficial <abol.mortal50@gmail.com>
2023-11-03 22:31:20 +0330

    remove not used codes

--- src/logger/static/js/main.js
+++ src/logger/static/js/main.js
@@ -2,26 +2,12 @@ $('#profile-dropdown-button').click(function () {
   $('#profile-dropdown').toggle(80, 'linear');
 });
 
-
-// $('#menuButton').click(function () {
-//   $('#menu').slideToggle(80);
-// });
-
-
 $(document).click(function (event) {
   var target = $(event.target);
   if (!target.closest('#profile-dropdown-button').length) {
     $('#profile-dropdown').hide(80, 'linear');
   }
 });
-
-// $(document).click(function (event) {
-//   var target = $(event.target);
-//   if (!target.closest('#menuButton').length) {
-//     $('#menu').slideUp(80);
-//   }
-// });
-
 
 $(document).ready(function () {
   var clipboard = new ClipboardJS("#shareProfile");
